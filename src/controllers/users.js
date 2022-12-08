2c62dd0bc38d50d066b55f058156f1778a3e1fc8
Taryana <118675343+Taryana89@users.noreply.github.com>
2022-12-09 05:28:12 +0700

    Testing

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -4,6 +4,7 @@ const { writeFile } = require("fs/promises")
 const { resolve } = require("path")
 const Sharp = require('sharp')
 const common = require('../helper/common')
+const cloudinary = require('../middlewares/caludinary')
 
 const controllersUser = {
     getUser: async (req, res, next) => {
@@ -29,30 +30,58 @@ const controllersUser = {
     createUser: async (req, res, next) => {
         try {
             const {name, mobile, usia, tanggal_lahir, address, education  } = req.body;
-            const fileImg = req.files.image[0].path
+            const fileImg1 = req.files.image[0].path
+            const fileImg2 = req.files.image[1] ? req.files.image[1].path : undefined;
             const filename = `watermarked-${Date.now()}.jpg`
-            if(fileImg){
-                Sharp(fileImg)
+            let datas = {
+                id: uuidv4(),
+                name, mobile, usia, tanggal_lahir, address, education,
+            }
+            if(fileImg1){
+                await Sharp(fileImg1)
                     .rotate()
                     .resize({
-                        width: 400,
-                        height: 400,
+                        width: 500,
+                        height: 500,
                     })
                     .jpeg({ mozjpeg: true })
                     .toBuffer()
                     .then(async (data) => {
                         await writeFile(resolve(`./src/uploads/${filename}`), data)
+                        const result = await cloudinary.uploader.upload(`./src/uploads/${filename}`)
+                        console.log(result.secure_url)
+                        datas = {
+                            ...datas,
+                            image1: fileImg1 ? result.secure_url : null
+                        }
+                        console.log(datas)
                     })
                     .catch(err => { console.log(err) })
-            }
-            const data = {
-                id: uuidv4(),
-                name, mobile, usia, tanggal_lahir, address, education,
-                image: fileImg ? `${process.env.API_BACKEND}img/${filename}` : null
-            }
-            modelUser.create(data)
-            common.response(res, data, "created success", 201 )
-
+                }
+                if(fileImg2){
+                await Sharp(fileImg2)
+                    .rotate()
+                    .resize({
+                        width: 1000,
+                        height: 1000,
+                    })
+                    .jpeg({ mozjpeg: true })
+                    .toBuffer()
+                    .then(async (data) => {
+                        await writeFile(resolve(`./src/uploads/${filename}`), data)
+                        const result = await cloudinary.uploader.upload(`./src/uploads/${filename}`)
+                        console.log(result.secure_url)
+                        datas = {
+                            ...datas,
+                            image2: fileImg2 ? result.secure_url : null
+                        }
+                        console.log(datas)
+                    })
+                    .catch(err => { console.log(err) })
+                }
+                console.log(datas)
+                await modelUser.create(datas)
+                common.response(res, datas, "created success", 201 )
         } catch (error) {
             console.log( error)
         }
@@ -68,28 +97,58 @@ const controllersUser = {
 
             }
             const { name, mobile, usia, tanggal_lahir, address, education } = req.body;
-            const fileImg = req.files.image[0].path
+            const fileImg1 = req.files.image[0].path
+            const fileImg2 = req.files.image[1] ? req.files.image[1].path : undefined;
             const filename = `watermarked-${Date.now()}.jpg`
-            if(fileImg){
-                Sharp(fileImg)
+            let datas = {
+                id: uuidv4(),
+                name, mobile, usia, tanggal_lahir, address, education,
+            }
+            if(fileImg1){
+                await Sharp(fileImg1)
                     .rotate()
                     .resize({
-                        width: 1000
+                        width: 500,
+                        height: 500,
                     })
                     .jpeg({ mozjpeg: true })
                     .toBuffer()
                     .then(async (data) => {
                         await writeFile(resolve(`./src/uploads/${filename}`), data)
+                        const result = await cloudinary.uploader.upload(`./src/uploads/${filename}`)
+                        console.log(result.secure_url)
+                        datas = {
+                            ...datas,
+                            image1: fileImg1 ? result.secure_url : null
+                        }
+                        console.log(datas)
                     })
                     .catch(err => { console.log(err) })
-            }
-            const datas = {
-                id,
-                name, mobile, usia, tanggal_lahir, address, education,
-                image: fileImg ? `${process.env.API_BACKEND}img/${filename}` : null
-            }
-            modelUser.update(datas)
-            common.response(res, datas, "updated success", 200 )
+                }
+                if(fileImg2){
+                await Sharp(fileImg2)
+                    .rotate()
+                    .resize({
+                        width: 1000,
+                        height: 1000,
+                    })
+                    .jpeg({ mozjpeg: true })
+                    .toBuffer()
+                    .then(async (data) => {
+                        await writeFile(resolve(`./src/uploads/${filename}`), data)
+                        const result = await cloudinary.uploader.upload(`./src/uploads/${filename}`)
+                        console.log(result.secure_url)
+                        datas = {
+                            ...datas,
+                            image2: fileImg2 ? result.secure_url : null
+                        }
+                        console.log(datas)
+                    })
+                    .catch(err => { console.log(err) })
+                }
+                console.log(datas)
+                await modelUser.update(datas)
+                common.response(res, datas, "created success", 201 )
         } catch (error) {
             console.log(error)
         }
